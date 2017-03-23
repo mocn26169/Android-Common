18892b7354898ec2f5272a7b90340952e0a2842a
mocn26169 <mocn26169@qq.com>
2017-03-23 23:59:44 +0800

    更新

--- app/src/main/java/com/bourne/android_common/ThreadDemo/ThreadPoolExecutorActivity.java
+++ app/src/main/java/com/bourne/android_common/ThreadDemo/ThreadPoolExecutorActivity.java
@@ -9,9 +9,12 @@ import com.bourne.android_common.R;
 import com.bourne.common_library.utils.Logout;
 
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -20,9 +23,8 @@ public class ThreadPoolExecutorActivity extends AppCompatActivity {
     private final int CORE_POOL_SIZE = 4;//核心线程数
     private final int MAX_POOL_SIZE = 5;//最大线程数
     private final long KEEP_ALIVE_TIME = 10;//空闲线程超时时间
-    private final int TOTALTICKET = 20;//总票数
     private final int BLOCK_SIZE = 2;//阻塞队列大小
-
+    private final int TOTALTICKET = 20;//总票数
 
     private ThreadPoolExecutor executorPool;
     private ExecutorService executorService;
@@ -35,36 +37,59 @@ public class ThreadPoolExecutorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_thread_pool_executor);
         // 创建线程池
         // 创建一个核心线程数为1、最大线程数为3的线程池
-        executorPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
+        ThreadPoolExecutor executorPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
                 TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(),
                 Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
 
         //当开启线程超过最大线程数量，就增加ArrayBlockingQueue里面规定的数量，再超过的话就抛出异常了
-//        executorPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
-//                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(BLOCK_SIZE),
-//                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
-//        executorPool.allowCoreThreadTimeOut(true);
+        ThreadPoolExecutor executorPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(BLOCK_SIZE),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        executorPool.allowCoreThreadTimeOut(true);
 
-        executorService= Executors.newFixedThreadPool(1);
+        executorService = Executors.newFixedThreadPool(1);
 
     }
 
-    private void useNewThread{
-        ExecutorService    singleTaskExecutor = Executors.newSingleThreadExecutor();// 每次只执行一个线程任务的线程池
-        ExecutorService      limitedTaskExecutor = Executors.newFixedThreadPool(3);// 限制线程池大小为7的线程池
-        ExecutorService   allTaskExecutor = Executors.newCachedThreadPool(); // 一个没有限制最大线程数的线程池
-        ExecutorService    scheduledTaskExecutor = Executors.newScheduledThreadPool(3);// 一个可以按指定时间可周期性的执行的线程池
-        ExecutorService     scheduledTaskFactoryExecutor = Executors.newFixedThreadPool(3, new ThreadFactoryTest());// 按指定工厂模式来执行的线程池
-        ExecutorService    scheduledTaskFactoryExecutor.submit(new Runnable()
-        {
+    public ThreadPoolExecutor(
+            int corePoolSize,
+            int maximumPoolSize,
+            long keepAliveTime,
+            TimeUnit unit,
+            BlockingQueue workQueue,
+            ThreadFactory threadFactory) {
 
-            @Override
-            public void run()
-            {
-                Log.i("KKK", "This is the ThreadFactory Test  submit Run! ! ! ");
-            }
-        });
+        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
+                threadFactory, defaultHandler);
     }
+
+    private void useNewThread
+
+    {
+        // 每次只执行一个线程任务的线程池
+        ExecutorService singleTaskExecutor = Executors.newSingleThreadExecutor();
+
+        // 限制线程池大小为7的线程池
+        ExecutorService limitedTaskExecutor = Executors.newFixedThreadPool(3);
+
+        // 一个没有限制最大线程数的线程池
+        ExecutorService allTaskExecutor = Executors.newCachedThreadPool();
+
+        // 一个可以按指定时间可周期性的执行的线程池
+        ExecutorService scheduledTaskExecutor = Executors.newScheduledThreadPool(3);
+
+        // 按指定工厂模式来执行的线程池
+        ExecutorService scheduledTaskFactoryExecutor = Executors.newFixedThreadPool(3, new ThreadFactoryTest());
+
+        ExecutorService scheduledTaskFactoryExecutor.submit(new Runnable() {
+
+        @Override
+        public void run() {
+            Log.i("KKK", "This is the ThreadFactory Test  submit Run! ! ! ");
+        }
+    });
+    }
+
     /**
      * 点击增加一个窗口
      *
