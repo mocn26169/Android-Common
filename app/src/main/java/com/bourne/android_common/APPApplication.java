7d636f0f6a6139e7e240e4637cc0fc6962392a87
mocn26169 <mocn26169@qq.com>
2017-04-24 22:17:42 +0800

    更新

--- app/src/main/java/com/bourne/android_common/APPApplication.java
+++ app/src/main/java/com/bourne/android_common/APPApplication.java
@@ -1,13 +1,38 @@
 package com.bourne.android_common;
 
-import com.bourne.common_library.base.BaseApplication;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.text.TextUtils;
 
-/**
- * Created by m on 2017/3/13.
- */
+import com.bourne.common_library.base.BaseApplication;
+import com.bourne.common_library.utils.Logout;
+
+import java.util.List;
+
+
 public class APPApplication extends BaseApplication{
     @Override
     public void onCreate() {
         super.onCreate();
+        String processName = getProcessName(this, android.os.Process.myPid());
+
+        if (!TextUtils.isEmpty(processName) && processName.equals(this.getPackageName())) {//判断进程名，保证只有主进程运行
+            //主进程初始化逻辑
+            Logout.e("onCreate");
+        }
+    }
+
+    public static String getProcessName(Context cxt, int pid) {
+        ActivityManager am = (ActivityManager) cxt.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runningApps = am.getRunningAppProcesses();
+        if (runningApps == null) {
+            return null;
+        }
+        for (ActivityManager.RunningAppProcessInfo procInfo : runningApps) {
+            if (procInfo.pid == pid) {
+                return procInfo.processName;
+            }
+        }
+        return null;
     }
 }
